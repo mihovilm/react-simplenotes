3a350f3cc742461d78b6dd6bbb9a5daeb31fd4bd
Mihovil Mandic <mihovil.mandic.21@dartmouth.edu>
2019-04-11 14:29:18 -0400

    working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // change require to es6 import style
 import $ from 'jquery';
+import './style.scss';
 
 let sec = 0;
-setInterval( () => {$('#main').text(`You've been on this page for ${sec++} seconds.`);}, 1000);
+setInterval(() => { $('#main').text(`You've been on this page for ${sec += 1} seconds.`); }, 1000);
