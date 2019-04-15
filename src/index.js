661ea8dca9c73e751bd3a8afc7b2750e8f287f97
Mihovil Mandic <mihovil.mandic.21@dartmouth.edu>
2019-04-15 13:53:04 -0400

    added React support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-// change require to es6 import style
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let sec = 0;
-setInterval(() => { $('#main').text(`You've been on this page for ${sec += 1} seconds.`); }, 1000);
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
